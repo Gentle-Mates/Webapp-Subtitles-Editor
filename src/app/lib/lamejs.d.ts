77bbe3421de9a8128c9d1965ead78fb7055ecbd6
Antoine <antoine@shaptwn.com>
2026-02-27 00:08:44 +0100

    build: update project

---
+++ src/app/lib/lamejs.d.ts
@@ -0,0 +1,9 @@
+declare module '@breezystack/lamejs' {
+    class Mp3Encoder {
+        constructor(channels: number, sampleRate: number, kbps: number);
+        encodeBuffer(left: Int16Array, right?: Int16Array): Uint8Array;
+        flush(): Uint8Array;
+    }
+
+    export { Mp3Encoder };
+}
